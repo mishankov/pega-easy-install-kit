aee80507638c1292870c956f0e9a8737de2a36fc
Denis Mishankov <mishankov@mail.com>
2022-08-02 22:28:02 +0300

    web-tail config generator

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cp distr/archives/prweb.war application/
 
 echo Initialize database
 docker-compose up -d database web-tail
+docker exec pega-logs node config-generator.js
 
 echo Run install script
 docker-compose up installer
@@ -12,5 +13,6 @@ docker-compose up installer
 echo Start containers
 docker-compose stop
 docker-compose up -d database application web-tail
+docker exec pega-logs node config-generator.js
 
 echo FINALE
---
+++ web-tail/config-generator.js
@@ -0,0 +1,29 @@
+const basePath = '/usr/src/app';
+const fs = require('fs');
+
+const logFolders = [
+    '/pega_logs',
+    '/database_logs',
+]
+
+const config = {
+    sources: []
+}
+
+logFolders.forEach(logFolder => {
+    const fullPath = basePath + logFolder;
+    try {
+        fs.readdirSync(fullPath).forEach(file => {
+            config.sources.push({
+                "name": file,
+                "type": "local:file",
+                "path": fullPath + '/' + file,
+            });
+        });
+    } catch (error) {
+        console.log(`Error reading ${fullPath}: ${error.toString()}`);
+    }
+    
+});
+
+fs.writeFileSync('/usr/src/app/dist/web-tail.config.json', JSON.stringify(config, null, 2));
