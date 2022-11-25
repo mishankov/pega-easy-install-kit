9f589b9cf9c18434b559aaef0433f9db34d59d86
Denis Mishankov <mishankov@mail.com>
2022-11-25 08:10:25 +0300

    pp

--- web-tail/config-generator.js
+++ web-tail/config-generator.js
@@ -1,32 +1,36 @@
-const basePath = '/usr/src/app';
-const fs = require('fs');
+const basePath = "/usr/src/app";
+const fs = require("fs");
 
 const logFolders = [
-    '/pega_logs',
-    // '/database_logs',
-]
+  "/pega_logs",
+  // '/database_logs',
+];
 
 const config = {
-    sources: []
-}
+  sources: [],
+};
 
-logFolders.forEach(logFolder => {
-    const fullPath = basePath + logFolder;
-    try {
-        fs.readdirSync(fullPath).filter(file => file.endsWith('.log')).forEach(file => {
-            config.sources.push({
-                "name": file,
-                "type": "local:file",
-                "path": fullPath + '/' + file,
-            });
+logFolders.forEach((logFolder) => {
+  const fullPath = basePath + logFolder;
+  try {
+    fs.readdirSync(fullPath)
+      .filter((file) => file.endsWith(".log"))
+      .forEach((file) => {
+        config.sources.push({
+          name: file,
+          type: "local:file",
+          path: fullPath + "/" + file,
         });
-    } catch (error) {
-        console.log(`Error reading ${fullPath}: ${error.toString()}`);
-    }
-    
+      });
+  } catch (error) {
+    console.log(`Error reading ${fullPath}: ${error.toString()}`);
+  }
 });
 
-console.log("Web-tail config: ")
-console.log(JSON.stringify(config, null, 2))
+console.log("Web-tail config: ");
+console.log(JSON.stringify(config, null, 2));
 
-fs.writeFileSync('/usr/src/app/dist/web-tail.config.json', JSON.stringify(config, null, 2));
+fs.writeFileSync(
+  "/usr/src/app/dist/web-tail.config.json",
+  JSON.stringify(config, null, 2)
+);
