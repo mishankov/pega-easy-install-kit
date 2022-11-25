eecea40f239f0ee1d354489c834eaccbb0aa52e7
Denis Mishankov <mishankov@mail.com>
2022-11-25 07:37:29 +0300

    move sctipts files

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,7 @@ echo Clone prweb.war
 cp distr/archives/prweb.war application/
 
 echo Initialize database
-docker-compose up -d database web-tail
-docker exec pega-logs node config-generator.js
+docker-compose up -d database
 
 echo Run install script
 docker-compose up installer
